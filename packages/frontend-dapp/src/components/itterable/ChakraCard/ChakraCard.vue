02e256477bd376ece553856bc47d717a26f70cfd
Christopher Maree <christopher.maree@gmail.com>
2020-10-11 23:43:59 +0200

    Added chakra card

---
+++ packages/frontend-dapp/src/components/itterable/ChakraCard/ChakraCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <md-card style="padding:30px">
+    <div class="md-layout">
+      <div class="md-layout-item md-size-20">
+        1
+      </div>
+      <div class="md-layout-item">
+        2
+      </div>
+    </div>
+    {{ chakraInfo }}
+    <Portfolio-Chart />
+  </md-card>
+</template>
+
+<style lang="scss" scoped>
+.card-expansion {
+  height: 480px;
+}
+.md-card {
+  width: 1000px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
+
+.box-text {
+  text-align: justify;
+  text-justify: inter-word;
+}
+
+// Link styling
+.a:link {
+  color: #af0404;
+  text-decoration: none;
+}
+
+/* visited link */
+.a:visited {
+  color: #fc6161;
+  text-decoration: underline;
+}
+
+/* mouse over link */
+.a:hover {
+  color: #fc6161;
+}
+
+/* selected link */
+.a:active {
+  color: #fc6161;
+  text-decoration: underline;
+}
+</style>
+<script>
+import { mapActions, mapState } from "vuex";
+
+import PortfolioChart from "./PortfolioChart";
+
+export default {
+  name: "ChakraCard",
+  components: { PortfolioChart },
+  data: () => ({ fomoIn: false, offer: 0 }),
+  props: {
+    chakraInfo: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    pageUrl() {
+      // return window.location.href;
+    }
+  },
+  methods: {
+    ...mapActions(["X"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.md-dialog {
+  max-width: 768px;
+}
+</style>
