16b178bb87a5d9445ffeed50c6229297ffa01774
Christopher Maree <christopher.maree@gmail.com>
2020-10-11 23:44:14 +0200

    Added portfolio component

---
+++ packages/frontend-dapp/src/components/itterable/ChakraCard/PortfolioChart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <div>
+      chart
+      <div class="main-section" style="padding-top: 20px">
+        <apexchart width="500" type="line" :options="options" :series="series"></apexchart>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue-apexcharts";
+
+export default {
+  name: "Home",
+  components: { VueApexCharts },
+  data() {
+    return {
+      options: {
+        chart: {
+          id: "vuechart-example"
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+      ]
+    };
+  }
+};
+</script>
