97f4738e23ffd86fe5c50ca38f8190e1509f5281
Christopher Maree <christopher.maree@gmail.com>
2020-10-17 14:43:49 +0200

    added coin modal

---
+++ packages/frontend-dapp/src/components/AddCoinModal.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-center">
+    <md-button class="md-primary md-raised" @click="showDialog = true">Show Dialog</md-button>
+
+    <md-dialog class="text-center" :md-active.sync="showDialog">
+      <md-dialog-title>Select Asset</md-dialog-title>
+      <md-dialog-content>
+        <Select-Asset-Row-Item v-for="token in allTokens" :asset="token" :key="token.address"></Select-Asset-Row-Item>
+      </md-dialog-content>
+    </md-dialog>
+  </div>
+</template>
+
+<script>
+/* global web3:true */
+import { mapActions, mapState } from "vuex";
+import SelectAssetRowItem from "@/components/itterable/SelectAssetRowItem";
+export default {
+  name: "AddCoinModal",
+  components: { SelectAssetRowItem },
+  data: () => ({
+    showDialog: true
+  }),
+  methods: {
+    ...mapActions(["CLOSE_MINING_DIALOG"]),
+    modalClosed() {
+      console.log("CLOSED");
+      this.CLOSE_MINING_DIALOG();
+    }
+  },
+  mounted() {},
+  computed: {
+    ...mapState(["allTokens"]),
+    allTokensInWallet() {
+      return [{ address: 1 }, { address: 2 }];
+    }
+  }
+};
+</script>
+
+<style></style>
