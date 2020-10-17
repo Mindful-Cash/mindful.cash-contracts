142dd1cb2b7ae599bfe3282a6c3a70bd56f70af6
Christopher Maree <christopher.maree@gmail.com>
2020-10-17 21:12:26 +0200

    Added sliders

--- packages/frontend-dapp/src/views/ChakraFlow.vue
+++ packages/frontend-dapp/src/views/ChakraFlow.vue
@@ -5,16 +5,43 @@
       <md-step id="first" md-label="Setup">
         <div class="left">
           <h2 class="title">Chakra Name</h2>
-          <p>
-            Choose a name for your Chakra. This will be viewable by other users.
-          </p>
+          <p>Choose a name for your Chakra. This will be viewable by other users.</p>
           <input placeholder="Enter Name" v-model="initial" />
           <Separator />
           <h2 class="title">Select Distribution</h2>
-          <p>
-            Select the tokens you want to add to your Chakra, and choose your distribution ratios.
-          </p>
-          <button class="add-asset-btn" @click="showTokenDialog = true"><span>+ Add Asset</span></button>
+          <p>Select the tokens you want to add to your Chakra, and choose your distribution ratios.</p>
+
+          <div v-for="coin in selectedCoins" :key="coin.address">{{ coin }}</div>
+          <div v-for="(coin, index) in selectedCoins" :key="index" style="padding-top: 20px">
+            <div class="md-layout md-gutter md-alignment-center-center">
+              <div class="md-layout-item md-size-15 md-layout">
+                <div class="md-layout-item">
+                  <img :width="30" :src="coin.logoURI" />
+                </div>
+                <div class="md-layout-item">
+                  <span class="secondaryText">({{ coin.symbol }}) </span>
+                </div>
+              </div>
+              <div class="md-layout-item">
+                <vue-slider
+                  v-model="coin.ratio"
+                  v-bind="selectorOptions"
+                  :dotOptions="{ max: coin.ratio + unselectedPercent }"
+                  :max="100"
+                  :tooltip="'always'"
+                  :process-style="{ backgroundColor: colors[index] }"
+                  :tooltip-style="{ backgroundColor: colors[index], borderColor: colors[index] }"
+                ></vue-slider>
+              </div>
+              <div class="md-layout-item md-size-10">
+                <md-button class="md-icon-button md-raised md-accent" @click="removeCoinFromSelected(index)">
+                  <md-icon>remove</md-icon>
+                </md-button>
+              </div>
+            </div>
+          </div>
+
+          <button class="add-asset-btn" @click="showCoinDialog = true"><span>+ Add Asset</span></button>
           <Separator />
           <h2 class="title">Initial Contribution</h2>
           <button>replace me</button><button>with switch</button>
@@ -60,10 +87,10 @@
         </p>
       </md-step>
     </md-steppers>
-    <md-dialog class="text-center roundedDialog" :md-active.sync="showTokenDialog">
+    <md-dialog class="text-center roundedDialog" :md-active.sync="showCoinDialog">
       <md-dialog-title class="selectAssets" style="text-align: left">Select Asset</md-dialog-title>
 
-      <md-dialog-content style="width:750px;padding-top:15px;padding-left:0px;padding-right:0px;">
+      <md-dialog-content style="width: 750px; padding-top: 15px; padding-left: 0px; padding-right: 0px">
         <Add-Coin-Modal @rowItemClicked="handelCoinChosen" />
       </md-dialog-content>
     </md-dialog>
@@ -78,16 +105,67 @@ export default {
   components: { Separator, AddCoinModal },
   data: () => ({
     initial: null,
-    showTokenDialog: false,
-    selectedCoins: []
+    showCoinDialog: false,
+    selectedCoins: [],
+    colors: [
+      "#A8A2F5",
+      "#E66C82",
+      "#F8D771",
+      "#9BECBE",
+      "#4371E0",
+      "#CC83E9",
+      "#F77D6A",
+      "#D5F871",
+      "#67E6ED",
+      "#7B66F7",
+    ],
   }),
   methods: {
     handelCoinChosen(coinObject) {
       console.log("clickedz", coinObject);
-      this.selectedCoins.push(coinObject);
-      this.showTokenDialog = false;
-    }
-  }
+      if (
+        this.selectedCoins
+          .map((itterationToken) => {
+            return itterationToken.symbol;
+          })
+          .indexOf(coinObject.symbol) == -1
+      ) {
+        this.selectedCoins.push({ ...coinObject, ratio: 0 });
+      } else {
+      }
+      this.showCoinDialog = false;
+    },
+    removeCoinFromSelected(removeIndex) {
+      console.log("removing", removeIndex);
+      this.selectedCoins[removeIndex].ratio = 0;
+      this.selectedCoins.splice(removeIndex, 1);
+      this.showCoinDialog = false;
+    },
+  },
+
+  computed: {
+    totalSelected() {
+      let total = 0;
+      this.selectedCoins.forEach(function (selected) {
+        if (selected.ratio) {
+          total += selected.ratio;
+        }
+      });
+      return total;
+    },
+    selectorOptions() {
+      let remaining = 100 - this.totalSelected;
+      return {
+        process: ([pos, i]) => [
+          [0, pos],
+          [pos, pos + remaining, { backgroundColor: "#999" }],
+        ],
+      };
+    },
+    unselectedPercent() {
+      return 100 - this.totalSelected;
+    },
+  },
 };
 </script>
 
@@ -202,5 +280,24 @@ h1.title {
   background: rgba(255, 255, 255, 0.25) !important;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 8px !important;
+}
+
+.primaryText {
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #292929;
+}
+
+.secondaryText {
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: right;
+  color: #aaaaaa;
 }
 </style>
