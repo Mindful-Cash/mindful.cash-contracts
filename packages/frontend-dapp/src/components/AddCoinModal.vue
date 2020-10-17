439fc6ec9a0df865284bf5eaa162b0f27ed41a98
Christopher Maree <christopher.maree@gmail.com>
2020-10-17 17:42:49 +0200

    updated modal styling

--- packages/frontend-dapp/src/components/AddCoinModal.vue
+++ packages/frontend-dapp/src/components/AddCoinModal.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="text-center">
+  <div class="text-center roundedDialog">
     <md-button class="md-primary md-raised" @click="showDialog = true">Show Dialog</md-button>
 
-    <md-dialog class="text-center" :md-active.sync="showDialog">
-      <md-dialog-title>Select Asset</md-dialog-title>
-      <md-dialog-content style="width:750px">
+    <md-dialog class="text-center roundedDialog" :md-active.sync="showDialog">
+      <md-dialog-title class="selectAssets" style="text-align: left">Select Asset</md-dialog-title>
+      <md-dialog-content style="width:750px;padding-top:15px">
         <Select-Asset-Row-Item v-for="token in allTokens" :asset="token" :key="token.address"></Select-Asset-Row-Item>
       </md-dialog-content>
     </md-dialog>
@@ -38,4 +38,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.selectAssets {
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 29px;
+  color: #292929;
+}
+
+.roundedDialog {
+  background: rgba(255, 255, 255, 0.25)  !important;;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 8px !important;
+}
+</style>
