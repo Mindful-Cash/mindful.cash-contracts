87e7083bedc523a43957d1980752400c59fb9eb5
chrisjgf <chrisjgf@gmail.com>
2020-10-21 23:06:08 +0200

    Landing - mobile, sections

--- packages/frontend-dapp/src/views/Landing.vue
+++ packages/frontend-dapp/src/views/Landing.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="landing md-layout">
     <div class="md-layout md-layout-item md-size-100" id="nav">
-      <div id="logo" class="md-layout-item md-size-80">
+      <div id="logo" class="md-layout-item ">
         <img src="@/assets/svg/logo.svg" alt="logo" />
       </div>
-      <button class="md-layout-item md-size-20"><span class="highlight">Go to app</span></button>
+      <div>
+        <button class="md-layout-item "><span class="highlight">Go to app</span></button>
+      </div>
     </div>
-    <div id="header" class="md-layout-item md-size-100">
+
+    <div id="header" class="md-layout-item md-size-100" style="background: yellow">
       <div class="md-layout md-gutter">
-        <div class="md-layout-item md-size-50">
+        <div class="md-layout-item md-size-50 md-xsmall-size-100">
           <h1 class="heading">Portfolio Alignment for Degens</h1>
           <p>
             Create auto-rebalancing portfolios and implement mindful DCA strategies while automatically taking profits
@@ -18,26 +21,26 @@
           <div style="display: flex; align-items: left">
             <button class="highlight-btn"><span>Align Your Chakras</span></button>
           </div>
-          <div class="separator-container" style="margin-top: 10rem">
+          <div class="separator-container" style="margin-top: 6rem">
             <div class="separator" />
           </div>
         </div>
         <div class="md-layout-item md-size-50">
-          <div id="lotus" />
+          <div class="md-xsmall-hide" id="lotus" />
         </div>
       </div>
     </div>
 
-    <div id="rebalance" class="section md-layout-item md-size-100">
+    <div id="rebalance" class="section md-layout-item md-size-100" style="background: green">
       <div class="md-layout md-gutter">
-        <div class="md-layout-item md-size-70">
+        <div class="md-layout-item md-size-70 md-xsmall-size-100">
           <h2 class="heading">Create Auto-Rebalancing Portfolios</h2>
         </div>
         <div class="md-layout-item md-size-30" />
       </div>
       <p>
-        Create bespoke portfolios - <span>”Chakras”</span> - when you’re feeling mindful. Your degen self will thank you
-        later.
+        Create bespoke portfolios - <span class="highlight">”Chakras”</span> - when you’re feeling mindful. Your degen
+        self will thank you later.
       </p>
       <div class="md-layout-item md-size-100" id="defi-energy" />
       <div class="md-layout-item md-size-100 separator-container center">
@@ -45,11 +48,12 @@
       </div>
     </div>
 
-    <div id="dca" class="section">
+    <div id="dca" class="section" style="background: blue">
       <div class="md-layout md-alignment-top-center">
         <h2 class="heading">Implement Mindful DCA Strategies</h2>
         <p class="md-layout-item md-size-100">
-          Allocate funds to a DCA pool, automate your strategy, and <span>never miss a bull run again.</span>
+          Allocate funds to a DCA pool, automate your strategy, and
+          <span class="highlight">never miss a bull run again.</span>
         </p>
         <div class="md-layout-item" id="chakra" />
         <div class="md-layout-item md-size-100 separator-container center">
@@ -59,12 +63,36 @@
     </div>
 
     <div class="section" id="chart">
-      <div class="md-layout md-alignment-top-center">
-        <h2 class="heading">Automatically Take Profits on the Way Up</h2>
-        <p class="md-layout-item md-size-100">
+      <div class="md-layout md-alignment-top-left">
+        <h2 class="md-layout-item md-size-80 md-xsmall-size-100 heading" style="text-align:left">
+          Automatically Take Profits on the Way Up
+        </h2>
+        <p class="md-layout-item md-size-60" style="text-align:left">
           Set automated parameters for taking profits to hedge against the whims of your
-          <span>degenerate future self.</span>
+          <span class="highlight">degenerate future self.</span>
         </p>
+      </div>
+    </div>
+
+    <div class="separator-container" style="margin-top: 6rem">
+      <div class="separator" />
+    </div>
+
+    <div id="guru" class="md-layout-item md-size-100" style="background: yellow">
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-50 md-xsmall-size-100">
+          <h2 class="heading">Find Your Guru</h2>
+          <p>
+            Browse and copy the <span class="highlight">most profitable strategies</span>, developed by the most mindful
+            traders.
+          </p>
+          <div style="display: flex; align-items: left">
+            <button class="highlight-btn"><span class="highlight">Find A Guru</span></button>
+          </div>
+        </div>
+        <div class="md-layout-item md-size-50">
+          <div class="md-xsmall-hide" id="guru-large" />
+        </div>
       </div>
     </div>
 
@@ -72,26 +100,12 @@
       <div class="separator" />
     </div>
 
-    <div class="section">
-      <div class="md-layout md-alignment-top-center">
-        <h2 class="heading">Find Your Guru</h2>
-        <p class="md-layout-item md-size-100">
-          Browse and copy the <span>most profitable strategies</span>, developed by the most mindful traders.
-        </p>
-        <button class="highlight-btn"><span>Find A Guru</span></button>
-      </div>
-    </div>
-
-    <div class="md-layout-item md-size-100 separator-container center">
-      <div class="separator" />
-    </div>
-
-    <div class="section">
+    <div class="section" style="background: blue">
       <div class="md-layout md-alignment-top-center">
         <h2 class="heading">Silence Your Inner Degenerate</h2>
         <p class="md-layout-item md-size-100">
           Start aligning your Chakras today. Don’t throw away your next opportunity to build
-          <span>generation wealth.</span>
+          <span class="highlight">generation wealth.</span>
         </p>
         <button class="highlight-btn"><span>Align Your Chakras</span></button>
       </div>
@@ -102,61 +116,40 @@
 </template>
 
 <style lang="scss">
-@import "@/styles/reset.scss";
 @import "@/styles/variables.scss";
 
 p {
   font-size: 1.25rem;
 }
 
-#dca p {
-  text-align: center;
+.landing {
+  background: #fff;
+  width: 100%;
+  height: 100%;
 }
 
-.heading {
-  font-size: 3.75rem;
-  font-weight: 600;
-  line-height: 4.5rem;
-  background: linear-gradient(74.67deg, #00e0ff -6.3%, #aa55ff 111.05%);
-  background-clip: text;
-  color: transparent;
-  margin-bottom: 3rem;
-}
-
-.highlight {
-  background: linear-gradient(74.67deg, #00e0ff -6.3%, #aa55ff 111.05%);
-  background-clip: text;
-  color: transparent;
-  margin: 1rem 0;
-}
-
-.highlight-btn {
-  background: linear-gradient(74.67deg, #00e0ff -6.3%, #aa55ff 111.05%);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border: none;
-  border-radius: 8px;
-  padding: 0.75rem 2rem;
-  margin: 2rem 0;
-
-  span {
-    color: #fff;
-    font-size: 1rem;
-    font-weight: 600;
-  }
+// Images
+#guru-large {
+  background: url("~@/assets/svg/guru-large.svg") no-repeat;
+  background-position-y: 25%;
+  background-position-x: 100%;
+  background-size: contain;
+  height: 100%;
 }
 
 #lotus {
   background: url("~@/assets/svg/lotus.svg") no-repeat;
-  background-position-y: -50%;
-  background-size: cover;
-  height: 80%;
+  background-position-y: 25%;
+  background-position-x: 100%;
+  background-size: contain;
+  height: 100%;
 }
 
 #chart {
   background: url("~@/assets/svg/stonks-up.svg") no-repeat;
-  background-position-y: 0;
-  background-size: contain;
-  height: 20rem;
+  background-position-y: 100%;
+  background-size: 100%;
+  height: 38rem;
 }
 
 #defi-energy {
@@ -184,22 +177,28 @@ p {
   margin-top: 4rem;
 }
 
-.separator-container {
+// Sections
+.section {
+  padding: 4rem 0 0 0;
+}
+
+#dca p {
+  text-align: center;
+}
+
+#guru {
   display: flex;
+  flex-direction: column;
+  height: 38rem;
 
-  height: 18px;
-  width: 100%;
-  margin: 3.25rem 0;
-}
+  p,
+  h2 {
+    text-align: left;
+  }
 
-.center {
-  justify-content: center;
-}
-
-.separator {
-  background: url("~@/assets/svg/three-dots.svg") no-repeat;
-  background-size: contain;
-  width: 5rem;
+  .heading {
+    margin-top: 4rem;
+  }
 }
 
 #nav {
@@ -233,6 +232,10 @@ p {
   h1 {
     text-align: left;
   }
+
+  .heading {
+    margin-top: 4rem;
+  }
 }
 
 #rebalance {
@@ -242,13 +245,53 @@ p {
   }
 }
 
-.section .heading {
-  padding: 4rem 0 0 0;
+// Defaults
+.heading {
+  font-size: 3.75rem;
+  font-weight: 600;
+  line-height: 4.5rem;
+  background: linear-gradient(74.67deg, #00e0ff -6.3%, #aa55ff 111.05%);
+  background-clip: text;
+  color: transparent;
+  margin: 0 0 3rem 0;
 }
 
-.landing {
-  background: #fff;
+.highlight {
+  background: linear-gradient(74.67deg, #00e0ff -6.3%, #aa55ff 111.05%);
+  background-clip: text;
+  color: transparent;
+  font-weight: 600;
+}
+
+.separator {
+  background: url("~@/assets/svg/three-dots.svg") no-repeat;
+  background-size: contain;
+  width: 5rem;
+}
+
+.highlight-btn {
+  background: linear-gradient(74.67deg, #00e0ff -6.3%, #aa55ff 111.05%);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
+  border-radius: 8px;
+  padding: 0.75rem 2rem;
+  margin: 2rem 0;
+
+  span {
+    color: #fff;
+    font-size: 1rem;
+    font-weight: 600;
+  }
+}
+
+.separator-container {
+  display: flex;
+  height: 18px;
   width: 100%;
-  height: 100%;
+  margin: 3.25rem 0;
+}
+
+.center {
+  justify-content: center;
 }
 </style>
